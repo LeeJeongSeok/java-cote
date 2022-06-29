632db54853ef3fd6c0552f92e7162403a126e2b9
LeeJeongSeok <ljs0429777@gmail.com>
2022-06-29 22:35:06 +0900

    feat : 봉우리 1차 풀이 미구현 부분 추가

--- src/main/java/me/jeongseok/cote/inflearn/Main2_10.java
+++ src/main/java/me/jeongseok/cote/inflearn/Main2_10.java
@@ -6,51 +6,63 @@ public class Main2_10 {
     public static int solution(int N, int[][] arr) {
 
         int answer = 0;
-        int[][] tempArr = new int[N][N];
 
         // 상, 하, 좌, 우 로직 비교
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
                 // 상
-                if (arr[i][j] > arr[i-1][j]) {
-                    if (tempArr[i-1][j-1] == 1) {
-                        break;
+                if (!isZero(arr[i-1][j])) {
+                    if (arr[i][j] > arr[i-1][j]) {
+                        arr[i][j] = 100;
+                        arr[i-1][j] = 0;
+                    } else {
+                        arr[i][j] = 0;
                     }
-                    tempArr[i-1][j-1] = 1;
                 }
                 // 하
-                if (arr[i][j] > arr[i+1][j]) {
-                    if (tempArr[i-1][j-1] == 1) {
-                        break;
+                if (!isZero(arr[i+1][j])) {
+                    if (arr[i][j] > arr[i+1][j]) {
+                        arr[i][j] = 100;
+                        arr[i-1][j] = 0;
+                    } else {
+                        arr[i][j] = 0;
                     }
-                    tempArr[i-1][j-1] = 1;
                 }
                 // 좌
-                if (arr[i][j] > arr[i][j-1]) {
-                    if (tempArr[i-1][j-1] == 1) {
-                        break;
+                if (!isZero(arr[i][j-1])) {
+                    if (arr[i][j] > arr[i][j-1]) {
+                        arr[i][j] = 100;
+                        arr[i][j-1] = 0;
+                    } else {
+                        arr[i][j] = 0;
                     }
-                    tempArr[i-1][j-1] = 1;
                 }
                 // 우
-                if (arr[i][j] > arr[i][j+1]) {
-                    if (tempArr[i-1][j-1] == 1) {
-                        break;
+                if (!isZero(arr[i][j+1])) {
+                    if (arr[i][j] > arr[i][j+1]) {
+                        arr[i][j] = 100;
+                        arr[i][j+1] = 0;
+                    } else {
+                        arr[i][j] = 0;
                     }
-                    tempArr[i-1][j-1] = 1;
                 }
             }
         }
 
-        for (int i = 0; i < tempArr.length; i++) {
-            for (int j = 0; j < tempArr.length; j++) {
-                System.out.print(tempArr[i][j] + " ");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i][j] == 100) {
+                    answer++;
+                }
             }
-            System.out.println(" ");
         }
         return answer;
     }
 
+    private static boolean isZero(int n) {
+        return n == 0;
+    }
 
 
     public static void main(String[] args) {
