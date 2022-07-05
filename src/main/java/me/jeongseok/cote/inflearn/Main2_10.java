c789749ce3368145b49762504f0e85c70cb538b7
LeeJeongSeok <ljs0429777@gmail.com>
2022-07-05 21:18:10 +0900

    feat : 봉우리 3차 힌트 주석 추가

--- src/main/java/me/jeongseok/cote/inflearn/Main2_10.java
+++ src/main/java/me/jeongseok/cote/inflearn/Main2_10.java
@@ -11,41 +11,34 @@ public class Main2_10 {
         // 애는 중첩포문으로 조건을 통과할때마다 계속 안으로 들어가야함
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
+                boolean result = true;
                 // 상
                 if (!isZero(arr[i-1][j])) {
-                    if (arr[i][j] > arr[i-1][j]) {
-                        arr[i][j] = 100;
-                        arr[i-1][j] = 0;
-                    } else {
-                        arr[i][j] = 0;
+                    if (arr[i-1][j] > arr[i][j]) {
+                        result = false;
                     }
                 }
                 // 하
                 if (!isZero(arr[i+1][j])) {
-                    if (arr[i][j] > arr[i+1][j]) {
-                        arr[i][j] = 100;
-                        arr[i-1][j] = 0;
-                    } else {
-                        arr[i][j] = 0;
+                    if (arr[i+1][j] > arr[i][j]) {
+                        result = false;
                     }
                 }
                 // 좌
                 if (!isZero(arr[i][j-1])) {
-                    if (arr[i][j] > arr[i][j-1]) {
-                        arr[i][j] = 100;
-                        arr[i][j-1] = 0;
-                    } else {
-                        arr[i][j] = 0;
+                    if (arr[i][j-1] > arr[i][j]) {
+                        result = false;
                     }
                 }
                 // 우
                 if (!isZero(arr[i][j+1])) {
-                    if (arr[i][j] > arr[i][j+1]) {
-                        arr[i][j] = 100;
-                        arr[i][j+1] = 0;
-                    } else {
-                        arr[i][j] = 0;
+                    if (arr[i][j+1] > arr[i][j]) {
+                        result = false;
                     }
+                }
+
+                if (result) {
+                    arr[i][j] = 100;
                 }
             }
         }
