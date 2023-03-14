c16049588d4de004fe437b8fee51817f8d38d490
Lee <ljs0429777@gmail.com>
2023-03-14 10:18:50 +0900

    완전탐색 풀이 추가

---
+++ src/main/java/me/jeongseok/cote/fastcampus/bruteforce_hard/BOJ_14888.java
@@ -0,0 +1,83 @@
+package me.jeongseok.cote.fastcampus.bruteforce_hard;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_14888 {
+
+	static int N, max, min;
+	static int[] nums, operator, order;
+	static StringBuilder sb = new StringBuilder();
+
+	static int calculator() {
+		int value = nums[1];
+
+		for (int i = 1; i <= N - 1; i++) {
+			if (order[i] == 1) {
+				value = value + nums[i + 1];
+			}
+			if (order[i] == 2) {
+				value = value - nums[i + 1];
+			}
+			if (order[i] == 3) {
+				value = value * nums[i + 1];
+			}
+			if (order[i] == 4) {
+				value = value / nums[i + 1];
+			}
+		}
+
+		return value;
+	}
+
+	static void rec_func(int k) {
+		if (k == N) {
+			int value = calculator();
+			max = Math.max(max, value);
+			min = Math.min(min, value);
+
+			sb.append(max).append("\n").append(min);
+		} else {
+			for (int cand = 1; cand <= 4; cand++) {
+				if (operator[cand] >= 1) {
+					operator[cand]--;
+					order[k] = cand;
+					rec_func(k + 1);
+					operator[cand]++;
+					order[k] = 0;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		input();
+
+		rec_func(1);
+		System.out.println(sb.toString());
+
+	}
+
+	private static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		max = Integer.MIN_VALUE;
+		min = Integer.MAX_VALUE;
+		nums = new int[N + 1];
+		operator = new int[5];
+		order = new int[N + 1];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= 4; i++) {
+			operator[i] = Integer.parseInt(st.nextToken());
+		}
+	}
+
+}
