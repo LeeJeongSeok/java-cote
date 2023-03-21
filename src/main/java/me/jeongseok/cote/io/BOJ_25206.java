1e1f62b9197cc6063acd09052418176a29ff89ad
Lee <ljs0429777@gmail.com>
2023-03-21 14:56:35 +0900

    백준 단계별 풀어보기 - 심화 풀이(너의 평점은) 답안 코드 추가

--- src/main/java/me/jeongseok/cote/io/BOJ_25206.java
+++ src/main/java/me/jeongseok/cote/io/BOJ_25206.java
@@ -9,28 +9,45 @@ public class BOJ_25206 {
 
 	static String grades[] = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F", "P"};
 	static double scores[] = {4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0, 0.0};
-	static double total = 0.0;
-	static double sumScore = 0.0;
+
+	static double gradeSum = 0.0;
+	static double scoreSum = 0.0;
+
+	static double getGradeScore(String grade) {
+		double gradeScore = 0.0;
+
+		for (int i = 0; i < grades.length; i++) {
+			if (grade.equals("P")) {
+				break;
+			}
+			if (grade.equals(grades[i])) {
+				gradeScore = scores[i];
+			}
+		}
+		return gradeScore;
+	}
+
+	static double getScoreSum(String grade, Double score) {
+		if (grade.equals("P")) {
+			return 0.0;
+		}
+		return score;
+	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+
 		for (int i = 0; i < 20; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			String subject = st.nextToken();
-			double score = Double.parseDouble(st.nextToken());
+			Double score = Double.parseDouble(st.nextToken());
 			String grade = st.nextToken();
 
-			for (int j = 0; j < 10; j++) {
-				if (grade.equals(grades[j])) {
-					total += scores[1] * scores[j];
-					if (j != 9) {
-						sumScore += score;
-					}
-				}
-			}
+			gradeSum += score * getGradeScore(grade);
+			scoreSum += getScoreSum(grade, score);
 		}
 
-		System.out.printf("%.6f\n", total / sumScore);
+		System.out.printf("%.6f\n", gradeSum / scoreSum);
 	}
 
 }
