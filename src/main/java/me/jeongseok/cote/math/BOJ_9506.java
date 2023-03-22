f367fc0132e165ff222244ca0c69ff8b97661866
Lee <ljs0429777@gmail.com>
2023-03-22 09:18:32 +0900

    백준 단계별 풀어보기 - 약수, 배수와 소수 풀이 코드 개선

--- src/main/java/me/jeongseok/cote/math/BOJ_9506.java
+++ src/main/java/me/jeongseok/cote/math/BOJ_9506.java
@@ -9,11 +9,48 @@ import java.io.InputStreamReader;
  * 12 is NOT perfect.
  * 28 = 1 + 2 + 4 + 7 + 14
  */
+
+/**
+ * 약수구하는 코드가 2번 반복됨
+ */
 public class BOJ_9506 {
 
+	static StringBuilder sb = new StringBuilder();
+
+	// idx 변수를 추가한 이유는 count + 1만큼 배열이 생성이 되고
+	// 1 ~ n까지 순회하면서 약수인지 판별할 때 순서에 맞게 배열에 집어넣기 위해 추가함
+	// 그렇지 않으면 0이 들어감
+	static int[] makeDivisorArray(int n, int count) {
+		int[] array = new int[count + 1];
+		int idx = 0;
+
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0) {
+				idx++;
+				array[idx] = i;
+			}
+		}
+
+		return array;
+	}
+	static boolean isPerfectNumber(int sum, int n) {
+		return sum == n;
+	}
+
+	static void printDivisorArray(int[] array, int count, int sum) {
+		sb.append(sum).append(" = ");
+
+		for (int i = 1; i <= count; i++) {
+			if (i < count) {
+				sb.append(array[i]).append(" + ");
+			} else {
+				sb.append(array[i]);
+			}
+		}
+		sb.append("\n");
+	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 
 		while (true) {
 			int n = Integer.parseInt(br.readLine());
@@ -33,25 +70,14 @@ public class BOJ_9506 {
 				}
 			}
 
-			// 구한 약수들을 더하기
-			int answer = 0;
-			if (sum == n) {
-				sb.append(sum).append(" = ");
-				for (int i = 1; i < n; i++) {
-					if (n % i == 0) {
-						answer++;
-						if (answer < count) {
-							sb.append(i).append(" + ");
-						}
-						if (answer == count) {
-							sb.append(i);
-						}
-					}
-				}
-				sb.append("\n");
-			} else {
+			if (!isPerfectNumber(sum, n)) {
 				sb.append(n + " is NOT perfect.").append("\n");
+			} else {
+				// 약수의 갯수 만큼 배열 생성한 후 배열에 insert
+				int[] divisorArray = makeDivisorArray(n, count);
+				printDivisorArray(divisorArray, count, sum);
 			}
+
 		}
 	}
 }
