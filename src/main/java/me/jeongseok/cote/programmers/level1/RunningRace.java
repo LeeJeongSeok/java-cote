b26efbf62b5725fe7df14aca5398de88f23a852d
Lee <ljs0429777@gmail.com>
2023-04-12 10:47:44 +0900

    feat : 프로그래머스 레벨 1 - 달리기 경주 성공 풀이 코드 추가

--- src/main/java/me/jeongseok/cote/programmers/level1/RunningRace.java
+++ src/main/java/me/jeongseok/cote/programmers/level1/RunningRace.java
@@ -1,37 +1,48 @@
 package me.jeongseok.cote.programmers.level1;
 
+import java.util.HashMap;
+
 /**
  * 프로그래머스 - 레벨1 - 달리기 경주
  */
 public class RunningRace {
 
-	public int findRunner(String[] players, String callName) {
-		int idx = 0;
+	public void swapRunner(HashMap<String, Integer> map, String calledPlayer) {
 
-		for (int i = 0; i < players.length; i++) {
-			if (callName.equals(players[i])) {
-				idx = i;
-			}
-		}
-
-		return idx;
-	}
-
-	public void swapRunner(String[] players, int idx) {
-		String temp = players[idx - 1];
-		players[idx - 1] = players[idx];
-		players[idx] = temp;
 	}
 
 	public String[] solution(String[] players, String[] callings) {
 		String[] answer = new String[players.length];
 
-		System.arraycopy(players, 0, answer, 0, players.length);
+		HashMap<String, Integer> mappedByPlayer = new HashMap<>();
+		HashMap<Integer, String> mappedByRank = new HashMap<>();
 
-		// calling의 배열 길이만큼 순회
+		// 각각의 맵을 초기화
+		for (int i = 0; i < players.length; i++) {
+			mappedByPlayer.put(players[i], i);
+			mappedByRank.put(i, players[i]);
+		}
+
 		for (int i = 0; i < callings.length; i++) {
-			int idx = findRunner(answer, callings[i]);
-			swapRunner(answer, idx);
+
+			// 추월한 유저 순위
+			// 추월한 유저 이름
+			int currentRank = mappedByPlayer.get(callings[i]);
+			String player = mappedByRank.get(currentRank);
+
+			// 바로 앞 플레이어
+			String frontPlayer = mappedByRank.get(currentRank - 1);
+
+			// swap
+			mappedByPlayer.put(player, currentRank - 1);
+			mappedByPlayer.put(frontPlayer, currentRank);
+
+			mappedByRank.put(currentRank - 1, player);
+			mappedByRank.put(currentRank, frontPlayer);
+		}
+
+		for (int i = 0; i < players.length; i++) {
+			answer[i] = mappedByRank.get(i);
 		}
 
 		return answer;
