efe05fd401abb231b133f82dd4c713ef9632705c
Lee <ljs0429777@gmail.com>
2023-04-17 09:09:58 +0900

    feat : 프로그래머스 레벨1 공원 산책 성공 풀이 코드 추가

--- src/main/java/me/jeongseok/cote/programmers/level1/ParkWalk.java
+++ src/main/java/me/jeongseok/cote/programmers/level1/ParkWalk.java
@@ -35,52 +35,73 @@ public class ParkWalk {
 
 			if (commands[0].equals("E")) {
 				// 공원 범위를 벗어나는지 체크, 장애물이 있는지 체크
+				boolean isTrue = true;
 
 				for (int j = 1; j <= Integer.parseInt(commands[1]); j++) {
-					int nx = j;
+					int nx = curX + j;
 
 					if (nx < 0 || nx >= park[0].length() || parkMap[curY][nx].equals("X")) {
-						continue;
+						isTrue = false;
+						break;
 					}
+				}
 
-					curX++;
+				if (isTrue) {
+					curX += Integer.parseInt(commands[1]);
 				}
 
 
 
 			} else if (commands[0].equals("W")) {
+
+				boolean isTrue = true;
+
 				for (int j = 1; j <= Integer.parseInt(commands[1]); j++) {
-					int nx = j;
+					int nx = curX - j;
 
 					if (nx < 0 || nx >= park[0].length() || parkMap[curY][nx].equals("X")) {
-						continue;
+						isTrue = false;
+						break;
 					}
+				}
 
-					curX--;
+				if (isTrue) {
+					curX -= Integer.parseInt(commands[1]);
 				}
 
 			} else if (commands[0].equals("N")) {
 
+				boolean isTrue = true;
+
 				for (int j = 1; j <= Integer.parseInt(commands[1]); j++) {
-					int ny = j;
+					int ny = curY - j;
 
 					if (ny < 0 || ny >= park.length || parkMap[ny][curX].equals("X")) {
-						continue;
+						isTrue = false;
+						break;
 					}
+				}
 
-					curY--;
+				if (isTrue) {
+					curY -= Integer.parseInt(commands[1]);
 				}
 
 
 			} else if (commands[0].equals("S")) {
+
+				boolean isTrue = true;
+
 				for (int j = 1; j <= Integer.parseInt(commands[1]); j++) {
-					int ny = j;
+					int ny = curY + j;
 
 					if (ny < 0 || ny >= park.length || parkMap[ny][curX].equals("X")) {
-						continue;
+						isTrue = false;
+						break;
 					}
+				}
 
-					curY++;
+				if (isTrue) {
+					curY += Integer.parseInt(commands[1]);
 				}
 			}
 		}
@@ -93,8 +114,8 @@ public class ParkWalk {
 
 	public static void main(String[] args) {
 //		new ParkWalk().solution(new String[]{"SOO","OOO","OOO"}, new String[]{"E 2","S 2","W 1"});
-		new ParkWalk().solution(new String[]{"SOO","OXX","OOO"}, new String[]{"E 2","S 2","W 1"});
-		new ParkWalk().solution(new String[]{"SOO","OOO","OOO"}, new String[]{"E 2","S 2","W 1"});
+//		new ParkWalk().solution(new String[]{"SOO","OXX","OOO"}, new String[]{"E 2","S 2","W 1"});
+		new ParkWalk().solution(new String[]{"OSO","OOO","OXO","OOO"}, new String[]{"E 2","S 3","W 1"});
 	}
 
 }
