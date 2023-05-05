d3a6348275b1f2d6000c5eb7016cd6e5741a014e
Lee <ljs0429777@gmail.com>
2023-05-05 15:09:43 +0900

    feat : 백트래킹 (n과 m 시리즈) 연습 코드 추가

---
+++ src/main/java/me/jeongseok/cote/math/BOJ_15649.java
@@ -0,0 +1,50 @@
+package me.jeongseok.cote.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15649 {
+
+	static int N, M;
+	static int[] selected;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void rec_func(int depth) {
+		if (depth == M + 1) {
+			for (int i = 1; i <= M; i++)
+				sb.append(selected[i]).append(" ");
+			sb.append("\n");
+		} else {
+			for (int i = 1; i <= N; i++) {
+				if (!visited[i]) {
+					visited[i] = true;
+					selected[depth] = i;
+					rec_func(depth + 1);
+					visited[i] = false;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		selected = new int[M + 1];
+		visited = new boolean[N + 1];
+
+
+		rec_func(1);
+
+		System.out.println(sb);
+
+
+	}
+
+}
