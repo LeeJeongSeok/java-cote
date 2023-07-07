238b1c682f19801f6c0580c63624f5536ebd50c8
Lee <ljs0429777@gmail.com>
2023-07-07 09:39:11 +0900

    feat : 2차원 BFS 탐색에 필요한 Pair 클래스 생성

---
+++ src/main/java/me/jeongseok/cote/pt_practice/Pair.java
@@ -0,0 +1,20 @@
+package me.jeongseok.cote.pt_practice;
+
+/**
+ * 4 6
+ * 101111
+ * 101010
+ * 101011
+ * 111011
+ */
+
+class Pair {
+	int x;
+	int y;
+
+	public Pair(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
