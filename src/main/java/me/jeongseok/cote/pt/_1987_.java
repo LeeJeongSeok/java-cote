8c63318229d678360acab35ba1a1b627b22a961f
Lee <ljs0429777@gmail.com>
2023-07-14 10:16:18 +0900

    feat : 그래프 탐색 - 알파벳 문제 풀이 추가

--- src/main/java/me/jeongseok/cote/pt/_1987_.java
+++ src/main/java/me/jeongseok/cote/pt/_1987_.java
@@ -5,12 +5,12 @@ import java.util.*;
 
 public class _1987_ {
 
-	static int R, C, temp;
+	static int R, C, max;
 	static int dx[];
 	static int dy[];
-	static String map[][];
-	static boolean visited[][];
-	static Set<String> set = new HashSet<>();
+	static int alphabet[];
+	static char board[][];
+
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,38 +19,39 @@ public class _1987_ {
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 
+		board = new char[R][C];
+		alphabet = new int[26];
 		dx = new int[]{1, 0, -1, 0};
 		dy = new int[]{0, 1, 0, -1};
-		map = new String[R][C];
-		visited = new boolean[R][C];
 
 		for (int i = 0; i < R; i++) {
-			String[] split = br.readLine().split("");
-			for (int j = 0; j < split.length; j++) {
-				map[i][j] = split[j];
+			char[] charArray = br.readLine().toCharArray();
+			for (int j = 0; j < charArray.length; j++) {
+				board[i][j] = charArray[j];
 			}
 		}
 
+		alphabet[board[0][0] - 65] = 1;
+		dfs(0, 0, 1);
+
+
+		System.out.println(max);
 	}
 
 	private static void dfs(int x, int y, int count) {
-		visited[x][y] = true;
-
 		for (int i = 0; i < 4; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 
-			if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
+			if (nx < 0 || nx >= R || ny < 0 || ny >= C || alphabet[board[nx][ny] - 65] == 1) {
 				continue;
 			}
 
-			if (visited[nx][ny] || set.contains(map[nx][ny])) {
-				continue;
-			}
-
-			set.add(map[nx][ny]);
+			alphabet[board[nx][ny] - 65] = 1;
 			dfs(nx, ny, count + 1);
-			temp = Math.max(count, temp);
+			alphabet[board[nx][ny] - 65] = 0;
 		}
+
+		max = Math.max(max, count);
 	}
 }
